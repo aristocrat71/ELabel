8b73ba479fc8bca822f150139a5bb1233b328b0b
aristocrat71 <sheth.mitul.71@gmail.com>
2025-07-10 20:10:42 +0530

    Added authentication headers

--- server/elabels/views.py
+++ server/elabels/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import ELabel
 from .serializers import ELabelSerializer
 
 # Create your views here.
 
 class ELabelViewSet(viewsets.ModelViewSet):
-    queryset = ELabel.objects.all()
     serializer_class = ELabelSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = ELabel.objects.filter(user=self.request.user)
+        company_profile_id = self.request.query_params.get('company_profile')
+        if company_profile_id:
+            queryset = queryset.filter(company_profile_id=company_profile_id)
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
